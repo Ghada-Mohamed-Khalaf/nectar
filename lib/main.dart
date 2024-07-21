66316d11654ea515943da4c8d4d82007e0e7ae26
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-07-21 19:33:39 +0300

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return  MaterialApp(
+    debugShowCheckedModeBanner: false,
+
+     home:Scaffold(
+
+     ),
 
     );
   }
